f7208fa4e0a913a795fcad2158c4a4930b3814ab
Hans <hanschristianyulianto@gmail.com>
2022-12-22 16:52:22 +0700

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Hans on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
